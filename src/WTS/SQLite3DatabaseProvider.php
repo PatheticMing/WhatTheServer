fc73c21691794ffe48018efe99c1a694948dc1e1
XdmingXD <mingming2101@gmail.com>
2017-08-09 23:19:51 +0800

    Update SQLite3DatabaseProvider.php

--- src/WTS/SQLite3DatabaseProvider.php
+++ src/WTS/SQLite3DatabaseProvider.php
@@ -11,12 +11,17 @@ class SQLiteDataProvider {
             
     public function __construct(wts $plugin) {
         $this->wts = $plugin;
-        $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
-        $this->database->exec("CREATE TABLE IF NOT EXISTS ServerLog
-                                        (id INTEGER PRIMARY KEY AUTOINCREMENT , date INTEGER , time INTEGER , player TEXT , 
-                                        level TEXT , x INTEGER , y INTEGER , z INTEGER , event TEXT , block TEXT , item_transfer TEXT , 
-                                        join_date INTEGER , last_join INETEGER , last_online INTEGER);");
-        $this->wts->database = $this->database;    
+        if(!file_exists($this->wts->getDataFolder() . "ServerLog.db")) {
+            $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
+            $this->database->exec("CREATE TABLE IF NOT EXISTS ServerLog
+                                            (id INTEGER PRIMARY KEY AUTOINCREMENT , date INTEGER , time INTEGER , player TEXT , 
+                                            level TEXT , x INTEGER , y INTEGER , z INTEGER , event TEXT , block TEXT , item_transfer TEXT , 
+                                            join_date INTEGER , last_join INETEGER , last_online INTEGER);");
+            $this->wts->database = $this->database;
+        } else{
+            $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
+            $this->wts->database = $this->database;
+        }
     }
     
 }
