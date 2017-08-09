c77c4e21369766a66ce0daf80b320cd45e8c2124
XdmingXD <mingming2101@gmail.com>
2017-08-09 17:07:44 +0800

    Create SQLite3DatabaseProvider.php

---
+++ src/WTS/SQLite3DatabaseProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WTS;
+
+use WTS\wts;
+use WTS\eventmanager;
+
+class SQLiteDataProvider {
+    
+    private $wts , $database;
+            
+    public function __construct(wts $plugin) {
+        $this->wts = $plugin;
+        $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
+        $this->database->exec("CREATE TABLE IF NOT EXISTS ServerLog
+                                        (id INTEGER PRIMARY KEY AUTOINCREMENT , date INTEGER , time INTEGER , player TEXT , 
+                                        level TEXT , x INTEGER , y INTEGER , z INTEGER , event TEXT , block TEXT , item_transfer TEXT , 
+                                        join_date INTEGER , last_join INETEGER , last_online INTEGER);");
+        $this->wts->database = $this->database;    
+    }
+    
+}
