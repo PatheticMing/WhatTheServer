ca8ead40d7f916eca7a13f30eae556b5b2173876
XdmingXD <mingming2101@gmail.com>
2017-08-09 17:08:11 +0800

    Update eventmanager.php

--- src/WTS/eventmanager.php
+++ src/WTS/eventmanager.php
@@ -12,14 +12,38 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\player\PlayerBucketEmptyEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\utils\TextFormat as C;
 
 class eventmanager extends PluginBase implements Listener {
     
-    private $wts;
+    private $wts , $datebase;
 
     public function __construct(wts $plugin) {
         $this->wts = $plugin;
+        
+    }
+    
+    public function onJoin(PlayerJoinEvent $event) {
+        $name = $event->getPlayer()->getName();
+        $time = $this->wts->getTime();
+        $date = $this->wts->getDate();
+        $query = $this->wts->getDatabase()->prepare("SELECT player FROM ServerLog WHERE player='$name' ");
+        $result = $query->execute();
+        var_dump($result);
+        $data = $this->wts->fetchall($result);
+        if($data == NULL) {
+        $this->wts->getDatabase()->exec("INSERT INTO ServerLog (date , time , player , join_date , last_join) VALUES ('$date' , '$time' , '$name' , '$date' , '$date')");
+        $this->wts->getLogger()->notice("Player '$name' not found!Data registered in datebase!");
+        } else{
+            $this->wts->database->exec("UPDATE ServerLog SET last_join='$time' WHERE player='$name' ");
+        }
+    }
+    
+    public function onQuit(PlayerQuitEvent $event) {
+        $name = $event->getPlayer()->getName();
+        $lastonline = date("Y-m-d H:i:s");
+        $this->wts->database->exec("UPDATE ServerLog SET last_online='$lastonline' WHERE player='$name' ");
     }
     
 }
