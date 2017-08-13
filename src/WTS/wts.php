f88c16108979017a12fa556925fb82352b856720
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-08-13 13:53:46 -0400

    Update wts.php

--- src/WTS/wts.php
+++ src/WTS/wts.php
@@ -5,6 +5,7 @@ namespace WTS;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat as C;
+use wts\Command\querycommand;
 
 class wts extends PluginBase {
     
