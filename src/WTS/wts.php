02b06f05076257c0d4842cafcab3a609a85ac51f
XdmingXD <mingming2101@gmail.com>
2017-08-15 00:16:29 +0800

    Update wts.php

--- src/WTS/wts.php
+++ src/WTS/wts.php
@@ -9,12 +9,16 @@ use pocketmine\utils\TextFormat as C;
 class wts extends PluginBase {
     
     public $database;
+    
+    private $data;
+
+    const WTS = "[WhatTheServer] ";
 
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents(new eventmanager($this), $this);
         $this->datebase = new SQLiteDataProvider($this);
         
-        $querycommand = new querycommand($this);
+        $querycommand = new \WTS\commands\querycommand($this);
         $this->getServer()->getCommandMap()->register($querycommand->command, $querycommand);
         
         $this->getLogger()->notice("This plugin is on BETA!Using SQLite3 data provider!");
@@ -43,8 +47,22 @@ class wts extends PluginBase {
         return date("Y-m-d");
     }
     
-    public function queryServerLog($player , $pos1 , $pos2 , $date , $time) {
-        
+    public function queryServerLog($player , $pos1 , $pos2 , $time) {
+        $X1 = floor($pos1->x);
+        $Y1 = floor($pos1->y);
+        $Z1 = floor($pos1->z);
+        $X2 = floor($pos2->x);
+        $Y2 = floor($pos2->y);
+        $Z2 = floor($pos2->z);
+        if($X1 < $X2 && $Y1 < $Y2 && $Z1 < $Z2) { //95 6 138 96 7
+            $query = $this->getDatabase()->prepare("SELECT id,date,time,player,x,y,z,event,block FROM ServerLog WHERE x>='$X1' AND x<='$X2' AND y>='$Y1' AND y<='$X2' AND z>='$Z1' AND z<='$Z2' ");
+            $result = $query->execute();
+            $data = $this->fetchall($result);
+            var_dump($data);
+            foreach($data as $i => $value) {
+                $player->sendMessage(C::YELLOW . wts::WTS . C::AQUA . "[" . $value["date"] . "] " . $value["time"] . C::GOLD . " '" . $value["player"] . "' " . C::RED . $value["event"] . " at" . C::GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"]);
+            }
+        }
     }
     
 }
