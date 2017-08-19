4993af30422e4cf7600a04b4f5b521755342a6c0
XdmingXD <mingming2101@gmail.com>
2017-08-19 19:58:04 +0800

    new namespace

---
+++ src/WhatTheServer/SQLiteDataProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WhatTheServer;
+
+use WhatTheServer\wts;
+use WhatTheServer\eventmanager;
+
+class SQLiteDataProvider {
+    
+    private $wts , $database;
+            
+    public function __construct(wts $plugin) {
+        $this->wts = $plugin;
+        if(!file_exists($this->wts->getDataFolder() . "ServerLog.db")) {
+            $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
+            $this->database->exec("CREATE TABLE IF NOT EXISTS ServerLog
+                                            (id INTEGER PRIMARY KEY AUTOINCREMENT , date INTEGER , time INTEGER , player TEXT , 
+                                            level TEXT , x INTEGER , y INTEGER , z INTEGER , event TEXT , block TEXT , blockid INTERGER , item_transfer TEXT , 
+                                            join_date INTEGER , last_join INETEGER , last_online INTEGER);");
+            $this->wts->database = $this->database;
+        } else{
+            $this->database = new \SQLite3($this->wts->getDataFolder() . "ServerLog.db");
+            $this->wts->database = $this->database;
+        }
+    }
+    
+}
