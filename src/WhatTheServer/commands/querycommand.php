79b2865f777b4688fb3f3a6d52a213637942bc75
XdmingXD <mingming2101@gmail.com>
2017-09-05 21:50:32 +0800

    Changed command

--- src/WhatTheServer/commands/querycommand.php
+++ src/WhatTheServer/commands/querycommand.php
@@ -14,30 +14,60 @@ use pocketmine\utils\TextFormat as C;
 
 class querycommand extends Command implements PluginIdentifiableCommand {
     
-    public $command = "query";
+    public $command = "wts";
     
     private $wts;
     
     public function __construct(wts $plugin) {
         parent::__construct($this->command, "Query what the server!");
-        $this->setUsage("/$this->command <x> <y> <z> <x2> <y2> <z2>  <time>");
+        $this->setUsage("Usage: /wts query <x> <y> <z> <x2> <y2> <z2> <time> | player <username>");
         $this->wts = $plugin;
     }
     
-    public function getPlugin() {
+    public function getPlugin(): \pocketmine\plugin\Plugin {
         return $this->wts;
     }
     
     public function execute(CommandSender $sender, $commandLabel, array $args) {
-        if(isset($args[0]) && isset($args[1]) && isset($args[2]) && isset($args[3]) && isset($args[4]) && isset($args[5]) && isset($args[6])) {
-            if(is_numeric($args[0]) && is_numeric($args[1]) && is_numeric($args[2]) && is_numeric($args[3]) && is_numeric($args[4]) && is_numeric($args[5]) && is_numeric($args[6])) {
-                $pos1 = array($args[0] , $args[1] , $args[2]);
-                $pos2 = array($args[3] , $args[4] , $args[5]);
-                $time = $args[6];
-                $this->getPlugin()->queryServerLog($sender , $pos1 , $pos2 , $time);
-            } else {
-                $sender->sendMessage($this->getUsage());
-                }
+        if(isset($args[0])) {
+            switch ($args[0]) {
+                case "query" :
+                    if($sender->hasPermission("yolo.command.query")) {
+                        if(isset($args[1]) && isset($args[2]) && isset($args[3]) && isset($args[4]) && isset($args[5]) && isset($args[6]) && isset($args[7])) {
+                            if(is_numeric($args[1]) && is_numeric($args[2]) && is_numeric($args[3]) && is_numeric($args[4]) && is_numeric($args[5]) && is_numeric($args[6]) && is_numeric($args[7])) {
+                                $pos1 = array($args[1] , $args[2] , $args[3]);
+                                $pos2 = array($args[4] , $args[5] , $args[6]);
+                                $time = $args[7];
+                                $this->getPlugin()->queryServerLog($sender , $pos1 , $pos2 , $time);
+                            } else {
+                                $sender->sendMessage($this->getUsage());
+                                }
+                        } else {
+                            $sender->sendMessage($this->getUsage());
+                        }
+                    } else {
+                        $sender->sendMessage(C::RED . "Sorry but you don't have permission to do that!");
+                    }
+                    break;
+                case "player" :
+                    if(isset($args[1])) {
+                        if($sender->hasPermission("yolo.command.query")) {
+                            if(isset($args[1])) {
+                                $this->getPlugin()->queryPlayer($sender , $args[1]);
+                            }
+                        }
+                    } else {
+                        $sender->sendMessage($this->getUsage());
+                    }
+                    break;
+                default:
+                    if($sender->hasPermission("yolo.command.query")) {
+                        $sender->sendMessage($this->getUsage());
+                    } else {
+                        $sender->sendMessage(C::RED . "Sorry but you don't have permission!");
+                    }
+                    break;
+            }
         } else {
             $sender->sendMessage($this->getUsage());
         }
