62a519c2b7711bf32dfb5718f8d0e2a6a01673d8
XdmingXD <mingming2101@gmail.com>
2019-02-03 21:26:58 +0800

    Displays records on book(s)
    Displays records on book(s) instead of displaying records in the chat

--- src/WhatTheServer/wts.php
+++ src/WhatTheServer/wts.php
@@ -4,6 +4,8 @@ namespace WhatTheServer;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
+use pocketmine\item\Item;
+use pocketmine\item\WrittenBook;
 use pocketmine\utils\TextFormat as C;
 
 class wts extends PluginBase {
@@ -25,7 +27,7 @@ class wts extends PluginBase {
         $querycommand = new \WhatTheServer\commands\querycommand($this);
         $this->getServer()->getCommandMap()->register($querycommand->command, $querycommand);
         
-        $this->getLogger()->notice("This plugin is on BETA!Using SQLite3 data provider!");
+        $this->getLogger()->notice("This plugin is in BETA! Using SQLite3 as the data provider!");
         $this->getLogger()->info(C::GOLD . "Loaded!");
     }
     
@@ -51,7 +53,7 @@ class wts extends PluginBase {
         return date("Y-m-d");
     }
     
-    public function queryServerLog($player , array $pos1 , array $pos2 /*, $time*/) {
+    public function queryServerLog($sender , array $pos1 , array $pos2 /*, $time*/) {
         $maxX = max($pos1[0] , $pos2[0]);
         $minX = min($pos1[0] , $pos2[0]);
         $maxY = max($pos1[1] , $pos2[1]);
@@ -61,17 +63,66 @@ class wts extends PluginBase {
         $query = $this->getDatabase()->prepare("SELECT id,date,time,player,x,y,z,event,block,blockid FROM ServerLog WHERE x BETWEEN '$minX' AND '$maxX' AND y BETWEEN '$minY' AND '$maxY' AND z BETWEEN '$minZ' AND '$maxZ' ");
         $result = $query->execute();
         $data = $this->fetchall($result);
-        if($data != null) {
-            foreach($data as $i => $value) {
-                $player->sendMessage(C::YELLOW . wts::WTS . C::AQUA . "[" . $value["date"] . "] " . $value["time"] . C::GOLD . " '" . $value["player"] . "' " . C::RESET . $value["event"] . " " . $value["block"] . "(" . $value["blockid"] . ")" . " at" . C::GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"]);
-            }
-        } else {
-            $player->sendMessage(C::YELLOW . wts::WTS . C::RED . "Cannot find any data!");
-        }
+		if($data != null) {
+			if($sender instanceof Player) {
+				$book = Item::get(Item::WRITTEN_BOOK, 0, 1);
+				$book->setTitle(C::YELLOW . C::UNDERLINE . 
+								"[pos 1: " . C::GREEN . $pos1[0] . C::YELLOW . ", " . C::GREEN . $pos1[1] . C::YELLOW . ", " . C::GREEN . $pos1[2] . C::YELLOW . "; " .
+								"pos 2: " . C::GREEN . $pos2[0] . C::YELLOW . ", " . C::GREEN . $pos2[1] . C::YELLOW . ", " . C::GREEN . $pos2[2] . C::YELLOW . "]");
+				$book->setAuthor(C::LIGHT_PURPLE . wts::WTS);
+				$temp = $text = [];
+				$j = $page = 0;
+				$totalcount = count($data);
+				$mod = fmod($totalcount, 2);
+				foreach($data as $i => $value) {
+					if(($page > 50) && $totalcount > 0) {
+						$sender->getInventory()->addItem($book);
+						$page = 0;
+					} else {
+						if(($j == 1)) {
+							$text[$page] = $temp[$i-1] . 
+											"(" . $i . ")\n" . C::BLUE . "[" . $value["date"] . "] " . $value["time"] . C::DARK_GREEN . " '" . $value["player"] . "' " . 
+											C::RED . $value["event"] . " " . C::DARK_GRAY . $value["block"] . "(" . $value["blockid"] . ")" . " at\n" . 
+											C::DARK_GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"] . "\n" . C::RESET;
+							$totalcount--;
+							$j = -1;
+							
+							$book->setPageText($page, $text[$page]);										
+							$page++;
+						} elseif(($totalcount < 2) && ($mod != 0)) {
+							$text[$page] = "(" . $i . ")\n" . C::BLUE . "[" . $value["date"] . "] " . $value["time"] . C::DARK_GREEN . " '" . $value["player"] . "' " . 
+											C::RED . $value["event"] . " " . C::DARK_GRAY . $value["block"] . "(" . $value["blockid"] . ")" . " at\n" . 
+											C::DARK_GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"] . "\n" . C::RESET;
+							$book->setPageText($page, $text[$page]);
+						} else {
+							$temp[$i] = "(" . $i . ")\n" .  C::BLUE . "[" . $value["date"] . "] " . $value["time"] . C::DARK_GREEN . " '" . $value["player"] . "' " . 
+										C::RED . $value["event"] . " " . C::DARK_GRAY . $value["block"] . "(" . $value["blockid"] . ")" . " at\n" . 
+										C::DARK_GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"] . "\n" . C::RESET;
+										$totalcount--;
+						}
+					}
+					$j++;
+				}
+				
+				$sender->getInventory()->addItem($book);
+				$sender->sendMessage(C::YELLOW . wts::WTS . C::AQUA . "Query has been done. " . C::RED . count($data) . C::AQUA . " records found!");
+				
+			} else {
+				$sender->sendMessage(C::RED . "------------------------------------------------------------------------------------------------------------");
+				foreach($data as $value) {
+					$sender->sendMessage(C::YELLOW . wts::WTS . C::AQUA . "[" . $value["date"] . "] " . $value["time"] . C::GOLD . " '" . $value["player"] . "' " . 
+										C::RESET . $value["event"] . " " . $value["block"] . "(" . $value["blockid"] . ")" . " at" . 
+										C::GREEN . " x= " . $value["x"] . " y= " . $value["y"] . " z= " . $value["z"]);
+				}
+			}
+		} else {
+			$sender->sendMessage(C::YELLOW . wts::WTS . C::RED . "Cannot find any data!");
+		}
     }
 
     public function queryPlayer($sender , $name) {
         $players = $this->getServer()->getOnlinePlayers();
+		$this->player = null;
         foreach($players as $player) {
             if(strtolower($player->getName()) == strtolower($name)) {
                 $this->player = $player->getName();
@@ -85,23 +136,19 @@ class wts extends PluginBase {
             $result = $query->execute();
             $data = $this->fetchall($result);
             if($data != null) {
-                foreach($data as $i => $value) {
-                    $sender->sendMessage(C::YELLOW . wts::WTS . "---------------\n" . C::GREEN . "Player : '$name' (Online) \n" . C::AQUA . 
-                    "Joined : " . $value["join_date"] . "\n" . 
-                    "Last seem : " . $value["last_join"] . " " . $value["last_online"]);
-                }
+                    $sender->sendMessage(C::YELLOW . wts::WTS . "---------------\n" . C::GREEN . "Player: '$name' (Online) \n" . C::AQUA . 
+                    "Joined: " . $data[0]["join_date"] . "\n" . 
+                    "Last seen: " . $data[0]["last_join"] . " " . $data[0]["last_online"]);          
             }
         } elseif($this->getServer()->getOfflinePlayer($name) != null) {
             $name = strtolower($name);
             $query = $this->getDatabase()->prepare("SELECT join_date,last_join,last_online FROM ServerLog WHERE player='$name' ");
             $result = $query->execute();
             $data = $this->fetchall($result);
-            if($data != null) {
-                foreach($data as $i => $value) {
-                    $sender->sendMessage(C::YELLOW . wts::WTS . "---------------\n" . C::RED . "Player : '$name' (Offline) \n" . C::AQUA . 
-                    "Joined : " . $value["join_date"] . "\n" . 
-                    "Last seem : " . $value["last_join"] . " " . $value["last_online"]);
-                }
+            if($data != null) {				
+                    $sender->sendMessage(C::YELLOW . wts::WTS . "---------------\n" . C::RED . "Player: '$name' (Offline) \n" . C::AQUA . 
+                    "Joined: " . $data[0]["join_date"] . "\n" . 
+                    "Last seen: " . $data[0]["last_join"] . " " . $data[0]["last_online"]);            
             } else {
                 $sender->sendMessage(C::YELLOW . wts::WTS . C::RED . "Cannot find any data!");
             }
